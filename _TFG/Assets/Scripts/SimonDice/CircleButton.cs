d6efe9fa11057a1857592981f744eb4c1a63d036
Diazmaster99 <147137420+Diazmaster99@users.noreply.github.com>
2025-10-07 13:05:08 +0200

    ColoresSimonDice

--- _TFG/Assets/Scripts/SimonDice/CircleButton.cs
+++ _TFG/Assets/Scripts/SimonDice/CircleButton.cs
@@ -6,23 +6,45 @@ public class CircleButton : MonoBehaviour
     public int index;
     private SpriteRenderer spriteRenderer;
     private Color originalColor;
+    public Color colorSimon;
+
+    
+    //public AudioClip sound;         
+    //private AudioSource audioSource;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        //audioSource = GetComponent<AudioSource>();
         originalColor = spriteRenderer.color;
     }
 
     void OnMouseDown()
-    {  
+    {
         if (SimonGameManager.Instance != null)
             SimonGameManager.Instance.OnCirclePressed(index);
+
+        // Reproduce sonido y efecto al tocar
+        //PlaySound();
+        StartCoroutine(Flash(0.2f));
     }
 
     public IEnumerator Flash(float duration)
     {
-        spriteRenderer.color = Color.yellow;
+        Color flashColor = colorSimon;
+        flashColor.a = 1f; // Asegurar que sea visible
+
+        spriteRenderer.color = flashColor;
+        //PlaySound();
         yield return new WaitForSeconds(duration);
         spriteRenderer.color = originalColor;
     }
+
+    /*private void PlaySound()
+    {
+        if (sound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(sound);
+        }
+    }*/
 }
