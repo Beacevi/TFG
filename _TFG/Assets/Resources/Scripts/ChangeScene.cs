0ba1c44a68b2bd7a3e486ff3fee506fd520f77d2
Andrés Sánchez Juárez <anfurtivo1@hotmail.com>
2026-02-10 20:25:42 +0100

    Arreglado bug escenas
    El CambiarEscenas desaparecía por lo que petaba y no te dejaba ir abajo mas de una vez

--- _TFG/Assets/Resources/Scripts/ChangeScene.cs
+++ _TFG/Assets/Resources/Scripts/ChangeScene.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
+    public static ChangeScene Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public void Cambiar_A_Escena(string nombreEscena)
     {
         SceneManager.LoadScene(nombreEscena);
