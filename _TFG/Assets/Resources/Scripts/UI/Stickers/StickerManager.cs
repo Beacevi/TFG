0bbd6b6d4b8ada07942b74a00ac850ad07169f78
Andrés Sánchez Juárez <anfurtivo1@hotmail.com>
2026-05-05 10:07:50 +0200

    Comentado metodo para cargar numero stickers

--- _TFG/Assets/Resources/Scripts/UI/Stickers/StickerManager.cs
+++ _TFG/Assets/Resources/Scripts/UI/Stickers/StickerManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class StickerManager : MonoBehaviour
@@ -26,4 +27,37 @@ public class StickerManager : MonoBehaviour
             selectedSticker.amount--;
         }
     }
+
+    //public void CargarRunas()
+    //{
+    //    var items = InventoryManager.instance.GetAvailableResources();
+
+    //    for (int i = 0; i < inventarioRutas.transform.childCount; i++)
+    //    {
+    //        inventarioRutas.transform.GetChild(i).gameObject.SetActive(false);
+    //    }
+
+    //    for (int i = 0; i < items.Count; i++)
+    //    {
+    //        if (i >= inventarioRutas.transform.childCount)
+    //            break;
+
+    //        Transform boton = inventarioRutas.transform.GetChild(i);
+    //        boton.gameObject.SetActive(true);
+
+    //        InventoryItem item = items[i];
+
+    //        // icono y texto
+    //        Image img = boton.GetComponentInChildren<Image>();
+    //        img.sprite = item.resource.displaySprite;
+
+    //        TMP_Text txt = boton.GetComponentInChildren<TMP_Text>();
+    //        txt.text = item.amount.ToString();
+
+    //        //AQUÍ ESTÁ LA CLAVE
+    //        RuneButton rune = boton.GetComponent<RuneButton>();
+    //        rune.Init(item.resource);
+    //    }
+    //}
+
 }
