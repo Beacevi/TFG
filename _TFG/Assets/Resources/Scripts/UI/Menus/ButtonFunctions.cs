06326dc7c64528a2a46ab7622d9e185a42e9fc42
Andrés Sánchez Juárez <anfurtivo1@hotmail.com>
2026-05-20 18:40:49 +0200

    Cambiada posicion botones pajaros Simon Says

--- _TFG/Assets/Resources/Scripts/UI/Menus/ButtonFunctions.cs
+++ _TFG/Assets/Resources/Scripts/UI/Menus/ButtonFunctions.cs
@@ -18,13 +18,19 @@ public class ButtonFunctions : MonoBehaviour
 
     public IEnumerator InteractibleButton(Button button, Animator animator)
     {
-        if(button)
+        if (button)
+        {
             button.interactable = false;
+        }
+            
         yield return null;
         float duration = animator.GetCurrentAnimatorStateInfo(0).length;
         yield return new WaitForSeconds(duration);
         if (button)
+        {
             button.interactable = true;
+        }
+            
     }
     public IEnumerator CloseInteractibleButton(Button button, Animator animator, GameObject panel)
     {
